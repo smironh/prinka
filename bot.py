afc0e0a8ec5ab905373f4d2af0eb6b9b1fa9310d
BEnZ <71022332+smironh@users.noreply.github.com>
2022-12-07 19:45:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ async def next(c: types.CallbackQuery):
     await bot.answer_callback_query(c.id)
 
     await c.message.delete()
-    if db.profile(c.msg)[0] < 5:
+    if db.profile(c.message)[0] < 5:
         await c.message.answer('У вас не достаточно рефералов!')
     else:
         await c.message.answer('Поздравляю, теперь у вас есть доступ!')
